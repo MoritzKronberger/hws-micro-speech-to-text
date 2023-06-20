754779bef1cad4e5fe5fdefa4c62c09c6a5058f9
Moritz Kronberger <moritz.kronberger99@gmail.com>
2023-06-20 15:40:35 +0200

    Improve preprocessing and preprocessing visualization

--- micro-stt/app/env.py
+++ micro-stt/app/env.py
@@ -17,14 +17,35 @@ def __from_dotenv(varname: str) -> str:
     return val
 
 
+# Audio recording
 DEVICE_SAMPLE_RATE = int(__from_dotenv('DEVICE_SAMPLE_RATE'))
 TARGET_SAMPLE_RATE = int(__from_dotenv('TARGET_SAMPLE_RATE'))
 BLOCK_SIZE = int(__from_dotenv('BLOCK_SIZE'))
 CHANNELS = int(__from_dotenv('CHANNELS'))
+
+# Model configuration
 LANGUAGE = __from_dotenv('LANGUAGE')
+
+# Audio preprocessing
+# Bandpass filter
+LOW_CUTOFF_FREQ = int(__from_dotenv('LOW_CUTOFF_FREQ'))
+HIGH_CUTOFF_FREQ = int(__from_dotenv('HIGH_CUTOFF_FREQ'))
+BANDPASS_Q = float(__from_dotenv('BANDPASS_Q'))
+# Noise reduction
+NOISE_REDUCE_STATIONARY = __from_dotenv('NOISE_REDUCE_STATIONARY') == 'True'
+NOISE_REDUCE_PROP_DECREASE = float(__from_dotenv('NOISE_REDUCE_PROP_DECREASE'))
 NOISE_FLOOR_DURATION_S = float(__from_dotenv('NOISE_FLOOR_DURATION_S'))
+# Scaling
+LEVEL_SCALE = float(__from_dotenv('LEVEL_SCALE'))
+# Overall configuration
+ENABLE_BANDPASS = __from_dotenv('ENABLE_BANDPASS') == 'True'
+ENABLE_NOISE_REDUCE = __from_dotenv('ENABLE_NOISE_REDUCE') == 'True'
+ENABLE_SCALE = __from_dotenv('ENABLE_SCALE') == 'True'
+
+# Performance benchmark configuration
 CPU_SPEED_GHZ = float(__from_dotenv('CPU_SPEED_GHZ'))
 
+# Helper constants
 NP_BUFFER = npt.NDArray[np.float32]
 OUT_PATH = 'out'
 IN_PATH = 'in'
